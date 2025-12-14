33d1cc37810534dde380b2eef94179f061fc2d97
WA-COO <tingwork.6@gmail.com>
2025-12-14 17:05:13 +0800

    Fix: Cloud Run 容器啟動失敗 (PORT 8080 修正)

--- server/index.js
+++ server/index.js
@@ -12,7 +12,7 @@ const User = require("./models/User");
 const Order = require("./models/Order");
 
 const app = express();
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 8080;
 
 // Middleware
 app.use(cors());
